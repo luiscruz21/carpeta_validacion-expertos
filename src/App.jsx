2e209e1b67d48ae8acf42d69bfbc39a8b6151c03
Luis Alfonso Cruz Galvez <luisalfonso210666@gmail.com>
2026-07-24 08:50:46 -0500

    Import useMemo in App.jsx to resolve ReferenceError blank screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react'
+import { useState, useEffect, useRef, useMemo } from 'react'
 import initialPreguntasData from './preguntas.json'
 import matrizData from './matriz_data.json'
 import consistenciaData from './consistencia_data.json'
